f325fd2cbff43f8338691b8f5841d00c2f555aad
schubert <me.scoob@gmail.com>
2013-02-19 16:29:50 +0530

    updated spec

--- spec/analytics_spec.rb
+++ spec/analytics_spec.rb
@@ -11,17 +11,41 @@ describe Rack::RedisAnalytics::Analytics do
   end
 
   before(:each) do
+    @redis_connection = Rack::RedisAnalytics.redis_connection
     clear_cookies
+    # @redis_connection.flushdb
   end
 
-  it "should set the tracker for visitor cookie" do
-    get '/'
-    last_response.original_headers.values.should be_any {|m| m =~ /#{Rack::RedisAnalytics.visit_cookie_name}=/}
+  context "when a visit has not timed out" do
+    it "should count as the same visit" do
+      t1 = Time.now
+      get '/'
+      last_response.original_headers['Set-Cookie'].should =~ /#{Rack::RedisAnalytics.visit_cookie_name}=1.#{t1.to_i}.#{t1.to_i}/
+      t2 = t1 + (Rack::RedisAnalytics.visit_timeout * 60) - 1
+      # Time.stubs(:now).returns(t2)
+      get '/'
+      last_response.original_headers['Set-Cookie'].should =~ /#{Rack::RedisAnalytics.visit_cookie_name}=1.#{t1.to_i}.#{t2.to_i}/
+    end
   end
   
-  it "should set the tracker for returning user cookie" do
-    get '/'
-    last_response.original_headers.values.should be_any {|m| m =~ /#{Rack::RedisAnalytics.returning_user_cookie_name}=/}
+  context "when the visit has timed out" do
+    it "should count as the next visit" do
+      t1 = Time.now
+      get '/'
+      last_response.original_headers['Set-Cookie'].should =~ /#{Rack::RedisAnalytics.visit_cookie_name}=1.#{t1.to_i}.#{t1.to_i}/
+      clear_cookies
+      t2 = Time.now
+      get '/'
+      last_response.original_headers['Set-Cookie'].should =~ /#{Rack::RedisAnalytics.visit_cookie_name}=2.#{t2.to_i}.#{t2.to_i}/
+    end
+
   end
+
+  it "should track new visits properly" do
+    get '/'
+    last_response.original_headers['Set-Cookie'].should =~ /#{Rack::RedisAnalytics.returning_user_cookie_name}=/
+  end
+  
+  
   
 end
