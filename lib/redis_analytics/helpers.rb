dce90395673c98ba1b9d4fe9c115c9d28aee3ad1
schubert <me.scoob@gmail.com>
2013-02-22 14:54:33 +0530

    visits browsers platforms and devices

--- lib/redis_analytics/helpers.rb
+++ lib/redis_analytics/helpers.rb
@@ -1,10 +1,14 @@
 module Rack
   module RedisAnalytics
     module Helpers
-      
+
+      FORMAT_SPECIFIER = [['%Y', 356], ['%m', 30], ['%d', 24], ['%H', 60 * 60]]
+      GRANULARITY = ['yearly', 'monthly', 'daily', 'hourly']
+
+      # all methods are private unless explicitly declared public
       private
       def method_missing(meth, *args, &block)
-        if meth.to_s =~ /^(hour|dai|month|year)ly_(new_visits|visits|unique_visits|visit_time)$/
+        if meth.to_s =~ /^(hour|dai|month|year)ly_(new_visits|visits|unique_visits|visit_time|ratio_browsers|ratio_platforms|ratio_devices)$/
           granularity = $1 + 'ly'
           type = $2
           data(granularity, type, *args)
@@ -13,63 +17,47 @@ module Rack
         end
       end
 
-      FS = [['%Y', 356], ['%m', 30], ['%d', 24], ['%H', 60 * 60]]
-      GR = ['yearly', 'monthly', 'daily', 'hourly']
-
       def data(granularity, type, from_date, to_date = Time.now)
-        seq = RedisAnalytics.incr("#{RedisAnalytics.redis_namespace}#SEQUENCER")
-        union_key = "#{RedisAnalytics.redis_namespace}#UNION##{key}##{seq}"
+
         i = from_date
         union = []
-        while i < to_date
-          i += FS[GR.index(granularity)..-1].map{|x| x[1]}.inject{|p,x| p*=x; p}
-          union << "#{RedisAnalytics.redis_namespace}#{i.strftime(FS[0..GR.index(granularity)].map{|x| x[0]}.join('_'))}"
+        begin
+          union << "#{Rack::RedisAnalytics.redis_namespace}:#{type}:#{i.strftime(FORMAT_SPECIFIER[0..GRANULARITY.index(granularity)].map{|x| x[0]}.join('_'))}"
+          i += FORMAT_SPECIFIER[GRANULARITY.index(granularity)..-1].map{|x| x[1]}.inject{|p,x| p*=x; p}
+        end while i <= to_date
+        puts union.inspect
+        if type =~ /unique/
+          Rack::RedisAnalytics.redis_connection.sunion(*union).length
+        elsif type =~ /ratio/
+          seq = get_next_seq
+          union_key = "#{Rack::RedisAnalytics.redis_namespace}:#{seq}"
+          Rack::RedisAnalytics.redis_connection.zunionstore(union_key, union)
+          Rack::RedisAnalytics.redis_connection.expire(union_key, 100)
+          Rack::RedisAnalytics.redis_connection.zrange(union_key, 0, -1, :with_scores => true)
+        else
+          Rack::RedisAnalytics.redis_connection.mget(*union).inject(0){|s, x| s += x.to_i; s}
         end
-        union
-        RedisAnalytics.redis_connection.zunionstore(union_key, union) 
-        RedisAnalytics.redis_connection.expire(union_key, 5 * 60) 
+      end
+      
+      def get_next_seq
+        seq = Rack::RedisAnalytics.redis_connection.incr("#{Rack::RedisAnalytics.redis_namespace}:#SEQUENCER")
       end
       
       # helpers for platform/browser
       def browsers(time = nil)
-        Rack::RedisAnalytics.redis_connection.zrange("#{RedisAnalytics.redis_namespace}UA_B_#{t.strftimex}", 0, -1, :with_scores => true)
+        Rack::RedisAnalytics.redis_connection.zrange("#{RedisAnalytics.redis_namespace}:UA_B_#{t.strftimex}", 0, -1, :with_scores => true)
       end
       
       # def browser_versions(time = nil)
-      #   Rack::RedisAnalytics.redis_connection.zrange("#{RedisAnalytics.redis_namespace}UA_B_V_#{t.strftime}", 0, -1, :with_scores => true)
+      #   Rack::RedisAnalytics.redis_connection.zrange("#{RedisAnalytics.redis_namespace}:UA_B_V_#{t.strftime}", 0, -1, :with_scores => true)
       # end
 
       def os_platforms(time = nil)
-        Rack::RedisAnalytics.redis_connection.zrange("#{RedisAnalytics.redis_namespace}UA_OS_#{t.strftime}", 0, -1, :with_scores => true)
+        Rack::RedisAnalytics.redis_connection.zrange("#{RedisAnalytics.redis_namespace}:UA_OS_#{t.strftime}", 0, -1, :with_scores => true)
       end
 
       def devices(time = nil)
-        Rack::RedisAnalytics.redis_connection.zrange("#{RedisAnalytics.redis_namespace}UA_D_#{t.strftime}", 0, -1, :with_scores => true)
-      end
-
-      # helpers for visits
-      def total_visits(time = nil)
-        
-      end
-
-      def total_new_visits(time = nil)
-        
-      end
-
-      def total_unqiue_visits(time = nil)
-        
-      end
-
-      def average_time_on_site(time = nil)
-        
-      end
-
-      def bounce_rate(time = nil)
-        
-      end
-
-      def activity
-        
+        Rack::RedisAnalytics.redis_connection.zrange("#{RedisAnalytics.redis_namespace}:UA_D_#{t.strftime}", 0, -1, :with_scores => true)
       end
 
     end
