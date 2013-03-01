64206b8e9a4291c6bca2365284871561cdbab3e0
schubert <me.scoob@gmail.com>
2013-03-01 11:26:13 +0530

    using mapper instead of analytics

--- lib/redis_analytics/railtie.rb
+++ lib/redis_analytics/railtie.rb
@@ -1,7 +1,13 @@
-module RedisAnalytics
-  class Railtie < Rails::Railtie
-    initializer "redis_analytics.insert_middleware" do |app|
-      app.config.middleware.use "Rack::RedisAnalytics::Analytics"
+module Rack
+  module RedisAnalytics
+    class Railtie < Rails::Railtie
+      initializer "redis_analytics.middleware" do |app|
+        app.config.middleware.use "Rack::RedisAnalytics::Mapper"
+      end
+      
+      initializer "redis_analytics.view_helpers" do
+        ActionController::Base.send :include, Rack::RedisAnalytics::Helpers
+      end
     end
   end
 end
