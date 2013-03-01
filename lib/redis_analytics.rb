67857ef987f7c98120ae570275935de87a24887b
schubert <me.scoob@gmail.com>
2013-03-01 11:28:28 +0530

    added dashboard and mapper

--- lib/redis_analytics.rb
+++ lib/redis_analytics.rb
@@ -1,12 +1,16 @@
 require 'rack'
 require 'redis'
-require 'useragent'
+require 'browser'
+require 'sinatra'
 
 require 'redis_analytics'
 require 'redis_analytics/version'
 require 'redis_analytics/configuration'
 require 'redis_analytics/analytics'
-require 'redis_analytics/railtie' if defined? Rails
+require 'redis_analytics/dashboard/dashboard'
+require 'redis_analytics/mapper'
+require 'redis_analytics/helpers'
+require 'redis_analytics/railtie' if defined? Rails::Railtie
 
 module Rack
   module RedisAnalytics
