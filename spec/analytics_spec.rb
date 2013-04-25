167eaa7b3862c1af98b4a120d245cc75416fa4f4
schubert <cardozoschubert@gmail.com>
2013-04-25 15:16:21 +0530

    spec update

--- spec/analytics_spec.rb
+++ spec/analytics_spec.rb
@@ -6,7 +6,7 @@ describe Rack::RedisAnalytics::Analytics do
   def app 
     Rack::Builder.app do
       use Rack::RedisAnalytics::Analytics
-      run Proc.new { |env| [200, {}, "Fuck you!"] }
+      run Proc.new { |env| [200, {'Content-Type' => 'text/html'}, "Hello!"] }
     end
   end
 
@@ -22,11 +22,11 @@ describe Rack::RedisAnalytics::Analytics do
     it "should count as the same visit" do
       t1 = Time.now
       get '/'
-      last_response.original_headers['Set-Cookie'].should =~ /#{Rack::RedisAnalytics.visit_cookie_name}=1.#{t1.to_i}.#{t1.to_i}/
+      last_response.original_headers['Set-Cookie'].should =~ Regexp.new("#{Rack::RedisAnalytics.visit_cookie_name}=1\.1\.#{t1.to_i}\.#{t1.to_i}.*")
       t2 = t1 + (Rack::RedisAnalytics.visit_timeout * 60) - 1
       Time.stubs(:now).returns(t2)
       get '/'
-      last_response.original_headers['Set-Cookie'].should =~ /#{Rack::RedisAnalytics.visit_cookie_name}=1.#{t1.to_i}.#{t2.to_i}/
+      last_response.original_headers['Set-Cookie'].should =~ Regexp.new("#{Rack::RedisAnalytics.visit_cookie_name}=1\.1\.#{t1.to_i}\.#{t2.to_i}.*")
     end
   end
   
@@ -34,11 +34,11 @@ describe Rack::RedisAnalytics::Analytics do
     it "should count as the next visit" do
       t1 = Time.now
       get '/'
-      last_response.original_headers['Set-Cookie'].should =~ /#{Rack::RedisAnalytics.visit_cookie_name}=1.#{t1.to_i}.#{t1.to_i}/
+      last_response.original_headers['Set-Cookie'].should =~ Regexp.new("#{Rack::RedisAnalytics.visit_cookie_name}=1\.1\.#{t1.to_i}.#{t1.to_i}.*")
       clear_cookies
       t2 = Time.now
       get '/'
-      last_response.original_headers['Set-Cookie'].should =~ /#{Rack::RedisAnalytics.visit_cookie_name}=2.#{t2.to_i}.#{t2.to_i}/
+      last_response.original_headers['Set-Cookie'].should =~ Regexp.new("#{Rack::RedisAnalytics.visit_cookie_name}=2\.2\.#{t2.to_i}\.#{t2.to_i}.*")
     end
 
   end
