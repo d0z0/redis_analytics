5014364822599213be32b7759fca1d162a15dbd7
gr4y <swessel@gr4yweb.de>
2013-06-03 23:18:00 +0200

    added an dashboard_spec, which lightly tests the dashboard rack app

---
+++ spec/dashboard_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe Rack::RedisAnalytics::Dashboard do
+
+  def app
+    Rack::RedisAnalytics::Dashboard
+  end
+
+  context 'the pretty dashboard' do
+
+    it 'should be mapped to configured endpoint' do
+      get Rack::RedisAnalytics.dashboard_endpoint
+      last_response.ok?
+    end
+
+    it 'should be content-type html' do
+      get Rack::RedisAnalytics.dashboard_endpoint
+      last_response.headers['Content-Type'] == "text/html"
+    end
+
+    it 'should contain the word Visits' do
+      get Rack::RedisAnalytics.dashboard_endpoint
+      last_response.body.include? "Visits"
+    end  
+
+  end
+
+end
