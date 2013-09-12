751bca7e6d9150d52c8273ab819cfb6453caa32f
schubert <cardozoschubert@gmail.com>
2013-09-12 12:46:23 +0530

    thread safety using dup.call using the should_record and filters from config

--- lib/redis_analytics/analytics.rb
+++ lib/redis_analytics/analytics.rb
@@ -2,20 +2,7 @@
 require 'digest/md5'
 module Rack
   module RedisAnalytics
-    
     class Analytics
-      
-      PAGEVIEWS = [['/health', 'o', 'd', 't'], ['/track', 'o', 'd', 't']]
-
-      TRANSACTIONS = {
-        "flights_search" => ["/flights/search", :o, :d],
-        "flights_results" => ["/flights/results", :o, :d],
-        "flights_book_step_1" => ["/flights/itinerary/:itinerary_id/info"],
-        "flights_book_step_2" => ["/flights/itinerary/:itinerary_id/traveller"],
-        "flights_book_step_3" => ["/flights/itinerary/:itinerary_id/pay"],
-        "flights_book_done" => ["/flights/itinerary/:itinerary_id/confirmation"],
-        "products" => ["/products/:id/review", :user_id]
-      }
 
       REFERRERS = ['google', 'bing', 'yahoo', 'cleartrip', 'github']
 
@@ -23,37 +10,28 @@ module Rack
         @app = app
         @redis_key_prefix = "#{RedisAnalytics.redis_namespace}:"
       end
-      
-      # def call(env)
-      #   dup.call!(env)
-      # end
 
       def call(env)
+        dup.call!(env)
+      end
+
+      def call!(env)
         @env = env
         @request  = Request.new(env)
         status, headers, body = @app.call(env)
         @response = Rack::Response.new(body, status, headers)
-        record if @response.ok? and @response.content_type =~ /^text\/html/
+        record if should_record?
         @response.finish
       end
 
-      def parse_path(path)
-        keys = []
-        spc = %w{. + ( )}
-        pattern =
-          path.to_str.gsub(/((:\w+)|[\*#{spc.join}])/) do |match|
-          case match
-          # when "*"
-          #   keys << 'splat'
-          #   "(.*?)"
-          when *spc
-            Regexp.escape(match)
-          else
-            keys << $2[1..-1]
-            "([^/?&#]+)"
+      def should_record?
+        return false unless @response.ok?
+        return false unless @response.content_type =~ /^text\/html/
+        RedisAnalytics.filters.each do |filter|
+          return false if filter.matches?(@request.path)
           end
         end
-        [/^#{pattern}$/, keys]
+        return true
       end
 
       def for_each_time_range(t)
@@ -67,25 +45,24 @@ module Rack
       end
 
       def record
-        t = Time.now 
+        t = Time.now
 
-        # record pageviews
-        path = @request.path
-        params = @request.params
-        if i = PAGEVIEWS.index{|x| x[0] == path}
-          page = PAGEVIEWS[i]
-          params.select{|x, y| page[1..-1].include?(x)}.each do |k, v|
-            for_each_time_range(t) do |ts, expire|
-              h = Digest::MD5.hexdigest(v)
-              RedisAnalytics.redis_connection.hset("#{@redis_key_prefix}page", h, v)
-              RedisAnalytics.redis_connection.expire("#{@redis_key_prefix}page", expire) if expire
+        # # Page Tracking
+        # path = @request.path
+        # params = @request.params
+        # if i = PAGEVIEWS.index{|x| x[0] == path}
+        #   page = PAGEVIEWS[i]
+        #   params.select{|x, y| page[1..-1].include?(x)}.each do |k, v|
+        #     for_each_time_range(t) do |ts, expire|
+        #       h = Digest::MD5.hexdigest(v)
+        #       RedisAnalytics.redis_connection.hset("#{@redis_key_prefix}page", h, v)
+        #       RedisAnalytics.redis_connection.expire("#{@redis_key_prefix}page", expire) if expire
+        #       RedisAnalytics.redis_connection.incr("#{@redis_key_prefix}page_#{i}_#{page.index(k)}_#{h}:#{ts}")
+        #       RedisAnalytics.redis_connection.expire("#{@redis_key_prefix}page_#{i}_#{page.index(k)}_#{h}:#{ts}", expire) if expire
+        #     end
+        #   end
+        # end
 
-              RedisAnalytics.redis_connection.incr("#{@redis_key_prefix}page_#{i}_#{page.index(k)}_#{h}:#{ts}")
-              RedisAnalytics.redis_connection.expire("#{@redis_key_prefix}page_#{i}_#{page.index(k)}_#{h}:#{ts}", expire) if expire
-            end
-          end
-        end
-        
         returning_visitor = @request.cookies[RedisAnalytics.returning_user_cookie_name]
         recent_visitor = @request.cookies[RedisAnalytics.visit_cookie_name]
 
@@ -120,12 +97,8 @@ module Rack
 
         # create the permanent cookie (2 years)
         @response.set_cookie(RedisAnalytics.returning_user_cookie_name, {:value => "#{rucn_seq}.#{first_visit_time}.#{t.to_i}", :expires => t + (60 * 60 * 24 * 5)}) # 5 hours temp
-
-        puts "TIME = [#{t}]"
-        puts "VISIT = #{vcn_seq}"
-        puts "UNIQUE VISIT = #{rucn_seq}"
       end
-      
+
       def new_visit(t)
         for_each_time_range(t) do |ts, expire|
           RedisAnalytics.redis_connection.incr("#{@redis_key_prefix}new_visits:#{ts}")
@@ -139,13 +112,12 @@ module Rack
         geo_country_code = nil
         referrer = nil
         ua = nil
-        
+
         # Geo IP Country code fetch
         if defined?(GeoIP)
           begin
             g = GeoIP.new(RedisAnalytics.geo_ip_data_path)
             geo_country_code = g.country(@request.ip).to_hash[:country_code2]
-            puts "Tracking IP #{@request.ip} using #{g.inspect} => GOT #{geo_country_code}"
           rescue Exception => e
             puts "Warning: Unable to fetch country info #{e}"
           end
@@ -168,7 +140,7 @@ module Rack
 
         # User agent
         ua = Browser.new(:ua => @request.user_agent, :accept_language => 'en-us')
-        
+
         for_each_time_range(t) do |ts, expire|
 
           # increment the total visits
@@ -178,33 +150,33 @@ module Rack
           if rucn_seq
             unique = RedisAnalytics.redis_connection.sadd("#{@redis_key_prefix}unique_visits:#{ts}", rucn_seq)
             RedisAnalytics.redis_connection.expire("#{@redis_key_prefix}unique_visits:#{ts}", expire) if expire
-            
+
             # geo ip tracking
             if geo_country_code and geo_country_code =~ /^[A-Z]{2}$/
               RedisAnalytics.redis_connection.zincrby("#{@redis_key_prefix}ratio_country:#{ts}", 1, geo_country_code)
               RedisAnalytics.redis_connection.expire("#{@redis_key_prefix}ratio_country:#{ts}", expire) if expire
             end
-            
+
             # referrer tracking 
             RedisAnalytics.redis_connection.zincrby("#{@redis_key_prefix}ratio_referrers:#{ts}", 1, referrer)
           end
-          
+
           # tracking for visitor recency
           if last_visit_time
             days_since_last_visit = ((t.to_i - last_visit_time)/(24*3600)).round
             RedisAnalytics.redis_connection.zincrby("#{@redis_key_prefix}ratio_recency:#{ts}", 1, days_since_last_visit)
             RedisAnalytics.redis_connection.expire("#{@redis_key_prefix}ratio_recency:#{ts}", expire) if expire
           end
-          
+
           RedisAnalytics.redis_connection.zincrby("#{@redis_key_prefix}ratio_browsers:#{ts}", 1,  ua.name)
           RedisAnalytics.redis_connection.expire("#{@redis_key_prefix}ratio_browsers:#{ts}", expire) if expire
 
           RedisAnalytics.redis_connection.zincrby("#{@redis_key_prefix}ratio_platforms:#{ts}", 1, ua.platform.to_s)
           RedisAnalytics.redis_connection.expire("#{@redis_key_prefix}ratio_platforms:#{ts}", expire) if expire
-          
+
           RedisAnalytics.redis_connection.zincrby("#{@redis_key_prefix}ratio_devices:#{ts}", 1, ua.mobile? ? 'M' : 'D')
           RedisAnalytics.redis_connection.expire("#{@redis_key_prefix}ratio_devices:#{ts}", expire) if expire
-        end        
+        end
       end
 
       def visit_time(t, visit_end_time = nil)
@@ -224,7 +196,6 @@ module Rack
           RedisAnalytics.redis_connection.expire("#{@redis_key_prefix}second_page_views:#{ts}", expire) if second_page_view and expire
         end
       end
-
 
     end
   end
