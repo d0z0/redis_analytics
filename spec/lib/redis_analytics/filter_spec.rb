27cb2f68ec8f79b8d7fef6a194d2e88669550833
schubert <cardozoschubert@gmail.com>
2013-09-13 11:41:02 +0530

    better filter spec

--- spec/lib/redis_analytics/filter_spec.rb
+++ spec/lib/redis_analytics/filter_spec.rb
@@ -9,7 +9,10 @@ describe Rack::RedisAnalytics::Filter do
 
   context "when a Filter matches" do
     it "should return true" do
-      filter = Rack::RedisAnalytics::Filter.new Proc.new {|req, res| req.ip == @mock_request.ip and res.content_type == @mock_response.content_type}
+      proc = Proc.new  do |req, res|
+        req.ip == @mock_request.ip and res.content_type == @mock_response.content_type
+      end
+      filter = Rack::RedisAnalytics::Filter.new(proc)
       filter.matches?(@mock_request, @mock_response).should be_true
     end
   end
@@ -17,6 +20,13 @@ describe Rack::RedisAnalytics::Filter do
   context "when a string PathFilter matches" do
     it "should return true" do
       filter = Rack::RedisAnalytics::PathFilter.new(@mock_request.path)
+      filter.matches?(@mock_request.path).should be_true
+    end
+  end
+
+  context "when a regexp PathFilter matches" do
+    it "should return true" do
+      filter = Rack::RedisAnalytics::PathFilter.new(/#{@mock_request.path}/)
       filter.matches?(@mock_request.path).should be_true
     end
   end
