0b6f63b6bd4c1c2592ee4ae4d58329a9c714faf4
schubert <cardozoschubert@gmail.com>
2013-10-13 20:28:16 +0530

    minor fixes in bounce rate and pageviews

--- lib/redis_analytics/visit.rb
+++ lib/redis_analytics/visit.rb
@@ -65,7 +65,7 @@ module Rack
           end
         end
         track("page_views", 1)
-        track("second_page_views", 1) if @last_visit_start_time == @last_visit_end_time
+        track("second_page_views", 1) if @last_visit_start_time and (@last_visit_start_time.to_i == @last_visit_end_time.to_i)
       end
 
       # helpers
