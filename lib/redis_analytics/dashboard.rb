27efa0924869c4e2e89181c0a79387c2b2170cf9
schubert <cardozoschubert@gmail.com>
2013-10-13 20:38:50 +0530

    removal of a lot of unwanted stuff again

--- lib/redis_analytics/dashboard.rb
+++ lib/redis_analytics/dashboard.rb
@@ -40,30 +40,11 @@ module Rack
       end
 
       get '/activity/?' do
-        with_benchmarking do
-          @data = {}
-        end
         erb :activity
       end
 
       get '/visits/?' do
-        with_benchmarking do
-          @range = time_range
-          # @data = {}
-
-          # RedisAnalytics.time_range_formats.each do |range, unit, time_format|
-          #   multiple = (1.send(range)/1.send(unit)).round
-          #   time_range = @t0 - 1.send(range) + 1.send(unit)
-
-          #   @data[range] ||= {}
-          #   visitor_recency = self.send("#{unit}ly_ratio_recency", time_range, :aggregate => true)
-          #   @data[range][:visitor_recency_slices] = [0, RedisAnalytics.visitor_recency_slices, '*'].flatten.each_cons(2).inject([]) do |h, (x, y)|
-          #     h << [[x, y], visitor_recency.select{|a, b| a.to_i >= x and (a.to_i < y  or y == '*') }.map{|p, q| q}.sum]
-          #   end
-          #   @data[range][:country_map] = Hash[self.send("#{unit}ly_ratio_country", time_range, :aggregate => true)]
-          # end
-          # @range = @data.keys[0] unless @data[@range]
-        end
+        @range = time_range
         erb :visits
       end
 
