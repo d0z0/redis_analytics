d7b80b2aca8a7b67224578f49eff47e49bf65dc9
Schubert <cardozoschubert@gmail.com>
2014-04-02 14:55:16 +0530

    added fixnum extensions

--- lib/redis_analytics/time_ext.rb
+++ lib/redis_analytics/time_ext.rb
@@ -25,3 +25,36 @@ end
 
 Time.send :include, TimeExtensions
 
+module FixnumExtensions
+
+  def minute
+    self * 60
+  end
+
+  def hour
+    minute * 60
+  end
+
+  def day
+    hour * 24
+  end
+
+  def week
+    day * 7
+  end
+
+  def month
+    day * 30 
+  end
+
+  def year
+    day * 365
+  end
+
+  def ago(time = Time.now)
+    time - self
+  end
+end
+
+Fixnum.send :include, FixnumExtensions
+
