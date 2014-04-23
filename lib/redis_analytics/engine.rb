63d0b73f252674c2f2269e899f7371ecc174b4b7
Schubert <cardozoschubert@gmail.com>
2014-04-23 15:52:14 +0530

    separated out engine assets by renaming manifest file

--- lib/redis_analytics/engine.rb
+++ lib/redis_analytics/engine.rb
@@ -3,16 +3,16 @@ require 'jquery-rails'
 
 module RedisAnalytics
   module Dashboard
-  class Engine < ::Rails::Engine
-    isolate_namespace RedisAnalytics
-    
-    initializer "redis_analytics.middleware" do |app|
-      app.config.app_middleware.use "RedisAnalytics::Tracker"
+    class Engine < ::Rails::Engine
+      isolate_namespace RedisAnalytics
+      
+      initializer "redis_analytics.middleware" do |app|
+        app.config.app_middleware.use "RedisAnalytics::Tracker"
+      end
+      
+      initializer "redis_analytics.view_helpers" do |app|
+        ActionController::Base.send :include, RedisAnalytics::Helpers
+      end
     end
-    
-    initializer "redis_analytics.view_helpers" do |app|
-      ActionController::Base.send :include, RedisAnalytics::Helpers
-    end
-  end
   end
 end
