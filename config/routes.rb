fcafd3b7535be649695ac33b185df0eac3facbb8
Dan Williams <dan@331.io>
2015-04-07 09:49:12 -0400

    Fix Issue #12 - Visits giving 404

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 RedisAnalytics::Dashboard::Engine.routes.draw do
   root :to => 'visits#index'
+  get '/visits', to: 'visits#index'
   get '/api/visits', to: 'api#visits'
   # get '/api/events', to: 'api#events'
 end
